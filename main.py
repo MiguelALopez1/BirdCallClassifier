2e798e5aebae95a5276533fee5f4cbde8bac9e93
MiguelALopez1 <miguel.a.lopez.083102@gmail.com>
2024-06-10 16:50:00 -0400

    update messages added to main

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ from scripts.train_model import train_model
 from scripts.evaluate_model import evaluate_model
 
 def main():
+    print("Loading data...")
     data_path = 'BirdSongsEurope/mp3/'
     df = load_data(data_path)
-
+    print("Data loading complete.")
+    
+    print("Preprocessing data...")
     df = preprocess_data(df)
-
+    print("Data preprocessing complete.")
+    
+    print("Training model...")
     trainer, test_dataset = train_model(df)
-
+    print("Model training complete.")
+    
     labels_list = sorted(list(df['label'].unique()))
-
+    
+    print("Evaluating model...")
     evaluate_model(trainer, test_dataset, labels_list)
-
+    print("Model evaluation complete.")
+    
     gc.collect()
+    print("Script execution complete.")
 
 if __name__ == "__main__":
     main()
